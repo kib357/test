d4e709a370d645add1a143e0e3ca9e7dc8156e2d
kib357 <kib357@outlook.com>
2016-11-13 15:57:11 +0500

    Info menu fix

--- src/js/components/InfoMenu.js
+++ src/js/components/InfoMenu.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import navListClasses from '../../css/navList.css';
+import componentClasses from '../../css/infoMenu.css';
 
 const InfoMenu = ({openPage}) => {
     const clickHandler = (e) => {
         const page = e.currentTarget.getAttribute('data-page');
         openPage(page);
     };
+
+    const stopPropagation = (e) => {
+        e.stopPropagation();
+    };
+
     return (
-        <div>
+        <div className={componentClasses.wrapper} onTouchTap={stopPropagation}>
             <ul className={navListClasses.list}>
                 <li onClick={clickHandler} data-page="">
                     <span>Услуги</span>
