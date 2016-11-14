eeec5991351c01e4b042a256c97ff6b6e507cfa7
kib357 <kib357@outlook.com>
2016-11-14 10:15:16 +0500

    Build

--- src/js/store/configureStore.js
+++ src/js/store/configureStore.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import createFetchMiddleware from '../middleware/fetch';
 import thunkMiddleware from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
+    let middleware = [createFetchMiddleware(), thunkMiddleware];
+
+    if (process.env.NODE_ENV !== 'production') {
+        const logger = require('redux-logger').createLogger();
+        middleware = [...middleware, logger];
+    }
+
     return createStore(
         rootReducer,
         initialState,
-        applyMiddleware(createFetchMiddleware(), thunkMiddleware, createLogger())
+        applyMiddleware(middleware)
     );
 }
